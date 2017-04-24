edc3250c6f266d06579d90c498db690d67d3c466
Sean <swu145@aucklanduni.ac.nz>
2017-04-24 15:01:30 +1200

    alu changed to combinational logic and proper overflow and zero bit

--- alu.vhd
+++ alu.vhd
@@ -8,64 +8,44 @@ use ieee.math_real.all;
 ---------------------------------------------------------------------------------------------------
 entity alu is
 -- generic and port declration here
-port(	clk			: 	in std_logic;
-		reset		: 	in std_logic;
+port(	
 		data_A		:	in std_logic_vector(15 downto 0);
 		data_B		:	in std_logic_vector(15 downto 0);
-		carry_in	:	in std_logic;
-		s_0			:	in std_logic;
-		s_1 		:	in std_logic;
-		s_2			:	in std_logic;
+		opcode		:	in std_logic_vector(3 downto 0);
 
-		data_out	:	out std_logic_vector(15 downto 0);
-		carry_out	:	out std_logic
+		data_out		:	out std_logic_vector(15 downto 0);
+		zero			:	out std_logic;
+		overflow		:	out std_logic
 		);
 end entity alu;
 
 ---------------------------------------------------------------------------------------------------
 architecture behaviour of alu is
-signal t_data_out : std_logic_vector(15 downto 0) := (others => '0');
-
+signal t_data_out : std_logic_vector(16 downto 0) := (others => '0');
 ---------------------------------------------------------------------------------------------------
--- component declaration here
 
 
 ---------------------------------------------------------------------------------------------------
 begin
--- component wiring here
 
+t_data_out <=  '0' & data_A 						when opcode = "0000" else
+'0' & (data_A + '1') 								when opcode = "0001" else
+('0' & data_A) + ('0' & data_B)					when opcode = "0010" else
+'0' & (data_A + data_B + 1) 						when opcode = "0011" else
+'0' & (data_A + (NOT data_B))		 				when opcode = "0100" else
+(('0' & data_A) + (NOT ('0' & data_B)) + '1')when opcode = "0101" else
+'0' & (data_A - '1')									when opcode = "0110" else
+'0' & data_B											when opcode = "0111" else
+'0' & (data_A AND data_B)							when opcode = "1000" else
+'0' & (data_A AND data_B) 							when opcode = "1001" else
+'0' & (data_A OR data_B)							when opcode = "1010" else
+'0' & (data_A OR data_B)							when opcode = "1011" else
+'0' & (data_A XOR data_B)							when opcode = "1100" else
+'0' & (NOT data_A)									when opcode = "1110" else
+'0' & (NOT data_B)									when opcode = "1111";
 
----------------------------------------------------------------------------------------------------
-alu_proc:	process(clk, reset)
-variable v_opcode : std_logic_vector(3 downto 0);
-begin
-	if( reset = '1') then
-		t_data_out <= (others => '0');
-	elsif (rising_edge(clk)) then
-		v_opcode := s_2 & s_1 & s_0 & carry_in;
-		case v_opcode is
-			when "0000" => t_data_out <= data_A;
-			when "0001" => t_data_out <= data_A + '1';
-			when "0010" => t_data_out <= data_A + data_B;
-			when "0011" => t_data_out <= data_A + data_B + 1;
-			when "0100" => t_data_out <= data_A + (NOT data_B);
-			when "0101" => t_data_out <= data_A + (NOT data_B) + '1'; --subtraction
-			when "0110" => t_data_out <= data_A - '1';
-			when "0111" => t_data_out <= data_B;
-			when "1000" => t_data_out <= data_A AND data_B;
-			when "1100" => t_data_out <= data_A AND data_B;
-			when "1001" => t_data_out <= data_A OR data_B;
-			when "1101" => t_data_out <= data_A OR data_B;
-			when "1010" => t_data_out <= data_A XOR data_B;
-			when "1110" => t_data_out <= NOT data_A;
-			when "1111" => t_data_out <= NOT data_B;
-			when others => t_data_out <= "X";
-		end case;
-	end if;
-end process alu_proc;
-
-
-data_out <= t_data_out;
-
+data_out <= t_data_out(15 downto 0);
+overflow <= t_data_out(16);
+zero <= '1' when data_A + (NOT data_B) + '1' = "0000000000000000" else '0';
 ---------------------------------------------------------------------------------------------------
 end architecture;
--- test_alu.vhd
+++ test_alu.vhd
@@ -9,42 +9,36 @@ architecture behaviour of test_alu is
 
 	component alu
 		port(
-			clk			: 	in std_logic;
-			reset		: 	in std_logic;
 			data_A		:	in std_logic_vector(15 downto 0);
 			data_B		:	in std_logic_vector(15 downto 0);
-			carry_in	:	in std_logic;
-			s_0			:	in std_logic;
-			s_1 		:	in std_logic;
-			s_2			:	in std_logic;
+			opcode		:	in std_logic_vector(3 downto 0);
 
-			data_out	:	out std_logic_vector(15 downto 0);
-			carry_out	:	out std_logic
+			data_out		:	out std_logic_vector(15 downto 0);
+			zero			:	out std_logic;
+			overflow		:	out std_logic
 		);
 	end component;
 
 
 
 	
-	signal t_clk, t_reset, t_carry_in, t_s_0, t_s_1, t_s_2, t_carry_out : std_logic := '0';
+	signal t_clk, t_reset, t_carry_in, t_overflow, t_zero : std_logic := '0';
 	signal t_data_A, t_data_B, t_data_out : std_logic_vector(15 downto 0) := (others => '0');
+	signal t_opcode : std_logic_vector(3 downto 0) := (others => '0');
 	constant t_clk_period : time := 20 ns;
 
 begin
 
 	t_alu : alu
 		port map(
-				clk 		=> 	t_clk,
-				reset		=> 	t_reset,
-				data_A		=> 	t_data_A,
-				data_B 		=> 	t_data_B,
-				carry_in	=> 	t_carry_in,
-				s_0			=>	t_s_0,
-				s_1			=>	t_s_1,
-				s_2			=> 	t_s_2,
+				data_A	=> 	t_data_A,
+				data_B 	=> 	t_data_B,
+				opcode	=>		t_opcode,
 
-				data_out	=>	t_data_out,
-				carry_out	=>	t_carry_out
+
+				data_out		=>	t_data_out,
+				zero 	=>	t_zero,
+				overflow	=>	t_overflow
 
 			);
 
@@ -59,19 +53,6 @@ begin
 		wait for t_clk_period/2;
 	end process;
 
-
-
-
-	t_reset_process : process
-	begin
-		t_reset <= '0';
-		wait for t_clk_period * 3;
-		t_reset <= '1';
-		wait for t_clk_period;
-		t_reset <= '0';
-		wait;
-	end process;
-
 	
 
 	t_alu_proc	:	process
@@ -81,117 +62,98 @@ begin
 		-- data_out <= data_A
 		t_data_A <= x"000F"; 
 		t_data_B <= x"0000";
-		t_s_2 <= '0';
-		t_s_1 <= '0';
-		t_s_0 <= '0';
-		t_carry_in <= '0';
+		t_opcode <= "0000";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A + 1
 		t_data_A <= x"000F"; 
 		t_data_B <= x"0000";
-		t_s_2		 <= '0';
-		t_s_1		 <= '0';
-		t_s_0		 <= '0';
-		t_carry_in	 <= '1';
+		t_opcode <= "0001";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A + data_B
 		t_data_A <= x"0002"; 
 		t_data_B <= x"0003";
-		t_s_2		 <= '0';
-		t_s_1		 <= '0';
-		t_s_0		 <= '1';
-		t_carry_in	 <= '0';
+		t_opcode <= "0010";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A + data_B + 1
 		t_data_A <= x"0002"; 
 		t_data_B <= x"0003";
-		t_s_2		<= '0';
-		t_s_1 		<= '0';
-		t_s_0 		<= '1';
-		t_carry_in 	<= '1';
+		t_opcode <= "0011";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A + (NOT data_B)
 		t_data_A <= x"0001"; 
 		t_data_B <= x"FFF0";
-		t_s_2 		<= '0';
-		t_s_1 		<= '1';
-		t_s_0 		<= '0';
-		t_carry_in 	<= '0';
+		t_opcode <= "0100";
 		wait for t_clk_period * 4;
 		-- data_out <= data_A + (NOT data_B) + '1'
 		t_data_A <= x"0006"; 
 		t_data_B <= x"0002";
-		t_s_2 <= '0';
-		t_s_1 <= '1';
-		t_s_0 <= '0';
-		t_carry_in <= '1';
+		t_opcode <= "0101";
 		wait for t_clk_period * 4;
 		-- data_out <= data_A - 1
 		t_data_A <= x"000F"; 
 		t_data_B <= x"0000";
-		t_s_2 <= '0';
-		t_s_1 <= '1';
-		t_s_0 <= '1';
-		t_carry_in <= '0';
+		t_opcode <= "0110";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_B
 		t_data_A <= x"0000"; 
 		t_data_B <= x"00F0";
-		t_s_2 <= '0';
-		t_s_1 <= '1';
-		t_s_0 <= '1';
-		t_carry_in <= '1';
+		t_opcode <= "0111";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A AND data_B
 		t_data_A <= x"000F"; 
 		t_data_B <= x"00F4";
-		t_s_2 <= '1';
-		t_s_1 <= '0';
-		t_s_0 <= '0';
-		t_carry_in <= '0';
+		t_opcode <= "1000";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A OR data_B
 		t_data_A <= x"000F"; 
 		t_data_B <= x"0000";
-		t_s_2 <= '1';
-		t_s_1 <= '0';
-		t_s_0 <= '0';
-		t_carry_in <= '1';
+		t_opcode <= "1010";
 		wait for t_clk_period * 4;
 
 		-- data_out <= data_A XOR data_B
 		t_data_A <= x"002F"; 
 		t_data_B <= x"0064";
-		t_s_2 <= '1';
-		t_s_1 <= '0';
-		t_s_0 <= '1';
-		t_carry_in <= '0';
+		t_opcode <= "1100";
 		wait for t_clk_period * 4;
 
 		-- data_out <= not data_A
 		t_data_A <= x"F00F"; 
 		t_data_B <= x"0000";
-		t_s_2 <= '1';
-		t_s_1 <= '1';
-		t_s_0 <= '1';
-		t_carry_in <= '0';
+		t_opcode <= "1110";
 		wait for t_clk_period * 4;
 
 		-- data_out <= not data_B
 		t_data_A <= x"000F"; 
 		t_data_B <= x"0000";
-		t_s_2 <= '1';
-		t_s_1 <= '1';
-		t_s_0 <= '1';
-		t_carry_in <= '1';
+		t_opcode <= "1111";
 		wait for t_clk_period * 4;
+
+		-- data_out <= data_A + data _B ; test overflow
+		t_data_A <= x"FFFF"; 
+		t_data_B <= x"FFFF";
+		t_opcode <= "0010";
+		wait for t_clk_period * 4;
+
+		-- data_out <= data_A - data_B; test zero bit
+		t_data_A <= x"00FF"; 
+		t_data_B <= x"00FF";
+		t_opcode <= "0101";
+		wait for t_clk_period * 4;
+
+		-- data_out <= data_A - data_B; test underflow
+		t_data_A <= x"000E"; 
+		t_data_B <= x"000F";
+		t_opcode <= "0101";
+		wait for t_clk_period * 4;
+
+
 
 		
 
