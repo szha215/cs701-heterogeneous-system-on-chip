2a2e2a4c9eb4882ec9c9e91c3f442a3f054dcd59
khdrew <andrewex.lai@hotmail.com>
2017-05-01 03:06:10 +1200

    data call implementation

--- recop_control.vhd
+++ recop_control.vhd
@@ -13,12 +13,14 @@ entity recop_control is
 port(	clk			: in std_logic;
 		am			: in std_logic_vector(1 downto 0);
 		opcode		: in std_logic_vector(5 downto 0);
-		
+		irq_flag	: in std_logic;
+			
 		m_addr_sel 	: out std_logic_vector(1 downto 0);
 		m_data_sel	: out std_logic_vector(1 downto 0);
 		m_wr		: out std_logic;
 		ir_wr		: out std_logic_vector(1 downto 0);
-		r_wr_sel	: out std_logic_vector(1 downto 0);
+		r_wr_d_sel	: out std_logic_vector(1 downto 0);
+		r_wr_r_sel	: out std_logic;
 		r_rd_sel	: out std_logic_vector(1 downto 0);
 		r_wr 		: out std_logic;
 		alu_src_A	: out std_logic_vector(1 downto 0);
@@ -26,6 +28,8 @@ port(	clk			: in std_logic;
 		alu_op		: out std_logic_vector(2 downto 0);
 		pc_src		: out std_logic_vector(1 downto 0);
 		set_EOT		: out std_logic;
+		reset_DPRR	: out std_logic;
+		reset_DPCR	: out std_logic;
 		reset_EOT	: out std_logic;
 		reset_ER	: out std_logic;
 		reset_Z		: out std_logic;
@@ -43,7 +47,7 @@ end entity recop_control;
 architecture behaviour of recop_control is
 -- type, signal, constant declarations here
 
-type states is (IF1, IF2, EX, CM);	-- states -- Instruction Fetch 1 & 2, Execute, Complete/Mem Acc
+type states is (IF1, IF2, EX, CM, DC);	-- states -- Instruction Fetch 1 & 2, Execute, Complete/Mem Acc
 signal CS, NS : states := IF1;
 
 ---------------------------------------------------------------------------------------------------
@@ -76,15 +80,27 @@ begin
 
 		when IF2 => -- Operand Fetch
 			NS <= EX;
+
 		when EX => -- Execute
-			if (opcode = and_op or opcode = or_op or opcode = add_op or 
-				opcode = subv_op or	opcode = present_op or opcode = max_op) then
+			if (opcode = dcallbl_op and irq_flag = '0') then
+				NS <= EX;
+			elsif (opcode = and_op or opcode = or_op or opcode = add_op or
+				opcode = subv_op or opcode = present_op or opcode = max_op) then
 				NS <= CM;
+			elsif (irq_flag = '1') then
+				NS <= DC;
 			else
 				NS <= IF1;
 			end if;
 
 		when CM => -- Complete/Memory Access
+			if (irq_flag = '1') then
+				NS <= DC;
+			else
+				NS <= IF1;
+			end if;
+			
+		when DC =>
 			NS <= IF1;
 
 		when others =>
@@ -100,13 +116,15 @@ begin
 
 	m_addr_sel 	<= "00";	m_data_sel	<= "00";
 	m_rd		<= '0';		m_wr		<= '0';
-	ir_wr		<= '0';		r_wr_sel	<= "000";
+	ir_wr		<= '0';		r_wr_d_sel	<= "000";
 	r_wr 		<= '0'; 	alu_src_A	<= "00";
 	alu_src_B	<= "00";	alu_op		<= "000";
 	pc_src		<= "00";	set_EOT		<= '0';
 	reset_EOT	<= '0';		reset_Z		<= '0';
+	reset_DPRR	<= '0';		reset_DPCR	<= '0';
 	pc_wr		<= '0';		pc_wr_cond	<= '0';
 	wr_SVOP		<= '0';		wr_SOP 		<= '0';
+	r_wr_r_sel	<= '0';
 
 	case CS is	-- must cover all states
 		when IF1 =>
@@ -165,17 +183,17 @@ begin
 				when ldr_op =>
 					case am is
 						when immediate_am =>
-							r_wr_sel <= "100";
+							r_wr_d_sel <= "100";
 							r_wr <= '1';
 						when register_am =>
 							m_rd <= '1';
 							m_addr_sel <= "11";
-							r_wr_sel <= "001";
+							r_wr_d_sel <= "001";
 							r_wr <= '1';
 						when direct_am =>
 							m_rd <= '1';
 							m_addr_sel <= "01";
-							r_wr_sel <= "001";
+							r_wr_d_sel <= "001";
 							r_wr <= '1';
 						when others =>
 							null;
@@ -213,9 +231,11 @@ begin
 					alu_src_B <= "10";
 					alu_op <= "011";
 
-				--when dcallbl_op =>
+				when dcallbl_op =>
+					wr_DPCR <= '1';
 
-				--when dcallbl_op =>
+				when dcallnb_op =>
+					wr_DPCR <= '1';
 
 				when sz_op =>
 					pc_wr_cond <= '1';
@@ -236,14 +256,14 @@ begin
 					reset_EOT <= '0';
 
 				when ler_op =>
-					r_wr_sel <= "010";
+					r_wr_d_sel <= "010";
 					r_wr <= '1';
 
 				when ssvop_op =>
 					wr_SVOP <= '1';
 
 				when lsip_op =>
-					r_wr_sel <= "011";
+					r_wr_d_sel <= "011";
 					r_wr <= '1';
 
 				when ssop_op =>
@@ -264,7 +284,18 @@ begin
 			end case;
 
 		when CM =>
-			null;
+			if (opcode = and_op or opcode = or_op or opcode = add_op or
+				opcode = subv_op or opcode = max_op) then
+				r_wr_d_sel <= "000";
+				r_wr <= '1';
+			elsif opcode = present_op then
+				pc_wr_cond <= '1';
+			end if;
+
+		when DC =>
+			reset_DPRR <= '1';
+			reset_DPCR <= '1';
+			-- write R0
 
 		when others =>
 			report "STATE OUTPUT: BAD STATE";
