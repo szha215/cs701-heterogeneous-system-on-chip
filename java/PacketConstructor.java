8a0ed13e20eaabc470da21131dd697659d76758b
Sean <swu145@aucklanduni.ac.nz>
2017-05-25 18:23:44 +1200

    updated packet constructor

--- java/PacketConstructor.java
+++ java/PacketConstructor.java
@@ -1,6 +1,9 @@
 /**
  * Created by seanwu on 25/05/17.
  */
+
+package test;
+
 public class PacketConstructor {
     private int printType = 0;
     private int opcode = 0;
@@ -15,31 +18,29 @@ public class PacketConstructor {
     private int data = 0;
 
     private int packet = 0;
-    public PacketConstructor(String opcodeStr, int destPort, int sourcePort, int arg2, int arg3){
+
+ 
+
+    public PacketConstructor(OPCODE.OP op, int destPort, int sourcePort, int arg2, int arg3){
         //arg2 and arg3 can be either memSel and numWords or endAddr and beginAddr depends on opcode
-        switch (opcodeStr){
-            case "STORE0":
-                opcode = 0x0;
-                break;
-            case "STORE1":
-                opcode = 0x1;
-                break;
-            case "XORA":
-                opcode = 0x2;
-                break;
-            case "XORB":
-                opcode = 0x3;
-                break;
-            case "MAC":
-                opcode = 0x4;
-                break;
-            case "AVEA":
-                opcode = 0x5;
-                break;
-            case "AVEB":
-                opcode = 0x6;
-                break;
+        
+        if(op == OPCODE.OP.STORE0){
+            this.opcode = 0x0;
+        }else if(op == OPCODE.OP.STORE1){
+            this.opcode = 0x1;
+        }else if(op == OPCODE.OP.XORA){
+            this.opcode = 0x2;
+        }else if(op == OPCODE.OP.XORB){
+
+            this.opcode = 0x3;
+        }else if(op == OPCODE.OP.MAC){
+            this.opcode = 0x4;
+        }else if(op == OPCODE.OP.AVEA){
+            this.opcode = 0x5;
+        }else if(op == OPCODE.OP.AVEB){
+            this.opcode = 0x6;
         }
+
 
         this.destPort = destPort;
         this.sourcePort = sourcePort;
@@ -49,7 +50,7 @@ public class PacketConstructor {
         //For other cmds
         this.endAddr = arg2;
         this.beginAddr = arg3;
-        buildPacket(opcodeStr);
+        buildPacket(opcode);
     }
 
     public PacketConstructor(int destPort, int lastBit, int addr, int data){
@@ -61,11 +62,11 @@ public class PacketConstructor {
         buildDataPacket();
     }
 
-    public void buildPacket(String opcodeStr){
-        if((opcodeStr == "STORE1") || (opcodeStr == "STORE0")) {
+    public void buildPacket(int opcode){
+        if((opcode == 0x0) | (opcode == 0x1)) {
             printType = 0;
             packet = (0x3 << 30) | (destPort << 26) | (opcode << 22) | (sourcePort << 18) | (memSel << 17) | (0x0 << 9) | (numWords << 0) & 0xFFFFFFFF;
-        }else if((opcodeStr == "XORA") || (opcodeStr == "XORB") || (opcodeStr == "MAC") || (opcodeStr == "AVEA") ||(opcodeStr == "AVEB")){
+        }else{
             printType = 1;
 
             packet = (0x3 << 30) | (destPort << 26) | (opcode << 22) | (sourcePort << 18) | (endAddr << 9) | (beginAddr << 0) & 0xFFFFFFFF;
@@ -111,28 +112,28 @@ public class PacketConstructor {
     }
 
     public static void main(String args[]){
-        PacketConstructor store0pkt = new PacketConstructor("STORE0",0x0,0x1,0x0,0x0);
+        PacketConstructor store0pkt = new PacketConstructor(OPCODE.OP.STORE0,0x0,0x1,0x0,0x0);
         store0pkt.printPacket();
 
-        PacketConstructor store1pkt = new PacketConstructor("STORE1",0x1,0x2,0x1,0x3);
+        PacketConstructor store1pkt = new PacketConstructor(OPCODE.OP.STORE1,0x1,0x2,0x1,0x3);
         store1pkt.printPacket();
 
-        PacketConstructor xorApkt = new PacketConstructor("XORA",0x3,0x4,0x5,0x4);
+        PacketConstructor xorApkt = new PacketConstructor(OPCODE.OP.XORA,0x3,0x4,0x5,0x4);
         xorApkt.printPacket();
 
-        PacketConstructor xorBpkt = new PacketConstructor("XORB",0x5,0x6,0x10,0x9);
+        PacketConstructor xorBpkt = new PacketConstructor(OPCODE.OP.XORB,0x5,0x6,0x10,0x9);
         xorBpkt.printPacket();
 
-        PacketConstructor macPkt = new PacketConstructor("MAC",0x7,0x8,0x20,0x15);
+        PacketConstructor macPkt = new PacketConstructor(OPCODE.OP.MAC,0x7,0x8,0x20,0x15);
         macPkt.printPacket();
 
-        PacketConstructor aveApkt = new PacketConstructor("AVEA",0x9,0x10,0x30,0x25);
+        PacketConstructor aveApkt = new PacketConstructor(OPCODE.OP.AVEA,0x9,0x10,0x30,0x25);
         aveApkt.printPacket();
 
-        PacketConstructor aveBpkt = new PacketConstructor("AVEB",0x11,0x12,0x40,0x35);
+        PacketConstructor aveBpkt = new PacketConstructor(OPCODE.OP.AVEB,0x11,0x12,0x40,0x35);
         aveBpkt.printPacket();
 
-        PacketConstructor dataPkt = new PacketConstructor(0x22,0x0,0x100,0x12);
+        PacketConstructor dataPkt = new PacketConstructor(0x22,0x1,0x100,0x12);
         dataPkt.printPacket();
 
 
