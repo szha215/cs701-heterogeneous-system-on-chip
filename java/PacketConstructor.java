a8ba26348f6384de8936e1548554a7fd74ce7a83
Sean Wu <swu145@aucklanduni.ac.nz>
2017-05-27 12:08:34 +1200

    added updatePacket method and fixed a bug in PacketConstructor

--- java/PacketConstructor.java
+++ java/PacketConstructor.java
@@ -22,8 +22,26 @@ public class PacketConstructor {
  
 
     public PacketConstructor(OPCODE.OP op, int destPort, int sourcePort, int arg2, int arg3){
+        parsePacketArgs(op,destPort,sourcePort,arg2,arg3);
+        buildPacket(opcode);
+    }
+
+    public PacketConstructor(int destPort, int lastBit, int addr, int data){
+        parsePacketArgs(destPort,lastBit,addr,data);
+
+        buildDataPacket();
+    }
+
+    private void parsePacketArgs(int destPort, int lastBit, int addr, int data){
+        this.destPort = destPort;
+        this.lastBit = lastBit;
+        this.addr = addr;
+        this.data = data;
+    }
+
+    private void parsePacketArgs(OPCODE.OP op, int destPort, int sourcePort, int arg2, int arg3){
         //arg2 and arg3 can be either memSel and numWords or endAddr and beginAddr depends on opcode
-        
+
         if(op == OPCODE.OP.STORE0){
             this.opcode = 0x0;
         }else if(op == OPCODE.OP.STORE1){
@@ -50,15 +68,15 @@ public class PacketConstructor {
         //For other cmds
         this.endAddr = arg2;
         this.beginAddr = arg3;
+    }
+
+    public void updatePacket(OPCODE.OP op, int destPort, int sourcePort, int arg2, int arg3){
+        parsePacketArgs(op,destPort,sourcePort,arg2,arg3);
         buildPacket(opcode);
     }
 
-    public PacketConstructor(int destPort, int lastBit, int addr, int data){
-
-        this.destPort = destPort;
-        this.lastBit = lastBit;
-        this.addr = addr;
-        this.data = data;
+    public void updatePacket(int destPort, int lastBit, int addr, int data){
+        parsePacketArgs(destPort,lastBit,addr,data);
         buildDataPacket();
     }
 
@@ -76,7 +94,7 @@ public class PacketConstructor {
     public void buildDataPacket(){
         printType = 2;
 
-        packet = (0x3 << 30) | (destPort << 26) | (lastBit << 25) | (addr << 15) | (data << 0) & 0xFFFFFFFF;
+        packet = (0x3 << 30) | (destPort << 26) | (lastBit << 25) | (addr << 16) | (data << 0) & 0xFFFFFFFF;
     }
 
     public int getPacket(){
@@ -115,6 +133,11 @@ public class PacketConstructor {
         PacketConstructor store0pkt = new PacketConstructor(OPCODE.OP.STORE0,0x0,0x1,0x0,0x0);
         store0pkt.printPacket();
 
+
+        store0pkt.updatePacket(OPCODE.OP.STORE1,0x1,0x2,0x1,0x3);
+        store0pkt.printPacket();
+
+
         PacketConstructor store1pkt = new PacketConstructor(OPCODE.OP.STORE1,0x1,0x2,0x1,0x3);
         store1pkt.printPacket();
 
@@ -134,6 +157,9 @@ public class PacketConstructor {
         aveBpkt.printPacket();
 
         PacketConstructor dataPkt = new PacketConstructor(0x22,0x1,0x100,0x12);
+        dataPkt.printPacket();
+
+        dataPkt.updatePacket(0x1,0x1,0x1,0x1);
         dataPkt.printPacket();
 
 
